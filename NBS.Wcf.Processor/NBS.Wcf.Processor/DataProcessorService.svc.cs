7ee6c67057caabff816f5b478bd87f3dc612d574
Abou Diaby <ab.diaby@gmail.com>
2020-05-08 10:46:08 -0500

    First commit

---
+++ NBS.Wcf.Processor/NBS.Wcf.Processor/DataProcessorService.svc.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Xml.Linq;
+
+namespace NBS.Wcf.Processor
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class DataProcessorService : IDataProcessorService
+    {
+        public XElement GetRequest(string processrequest)
+        {
+            return XElement.Parse("<Service><Notification> Service on and live</Notification></Service>");
+        }
+
+        public void processrequest(XElement processrequest)
+        {
+            var FileName = $@"{DateTime.Now.Ticks}.xml";
+            try
+            {
+                string root = @"C:\NBSData";
+                // If directory does not exist, don't even try   
+                if (!Directory.Exists(root))
+                {
+                    Directory.CreateDirectory(root);
+                }
+                string file = string.Format("{0}\\{1}", root, FileName);
+                processrequest.Save(file); 
+            }
+            catch (Exception exec)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ NBS.Wcf.Processor/NBS.Wcf.Processor/IDataProcessorService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Xml.Linq;
+
+namespace NBS.Wcf.Processor
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IDataProcessorService
+    {
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/{processrequest}", ResponseFormat = WebMessageFormat.Xml, BodyStyle = WebMessageBodyStyle.Bare)]
+        XElement GetRequest(string processrequest);
+
+        [OperationContract]
+        [WebInvoke(Method ="POST", UriTemplate ="/processrequest", RequestFormat =WebMessageFormat.Xml,BodyStyle =WebMessageBodyStyle.Bare, ResponseFormat = WebMessageFormat.Xml)]
+        void processrequest( XElement processrequest);
+
+        // TODO: Add your service operations here
+    }
+
+
+   
+    }
+
